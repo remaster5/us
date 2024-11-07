9bfb3a88c0b5c567c8c3b62edb58c8ab7f83c9b0
remaster5 <right.stock.hunter@gmail.com>
2024-11-07 15:54:40 +0900

    Update and rename calc-sp500-rs.py to calc-nyse-rs.py

--- calc-nyse-rs.py
+++ calc-nyse-rs.py
@@ -12,15 +12,15 @@ import datetime as dt
 import textwrap
 
 
-LIST_FILENAME = "sp500-list.csv"
+LIST_FILENAME = "nyse-list.csv"
 TARGET = 'S&P500'
 DATA_DIR_ROOT = "DATA"
 
-print("SP500 리스트를 가져옵니다.")
-sp500_list = fdr.StockListing(TARGET)
-sp500_list.to_csv(LIST_FILENAME)
+print("NYSE 리스트를 가져옵니다.")
+nyse_list = fdr.StockListing(TARGET)
+nyse_list.to_csv(LIST_FILENAME)
 
-print(sp500_list.shape)
+print(nyse_list.shape)
 
 now = dt.datetime.now()
 date = now.strftime("%Y-%m-%d")
@@ -28,7 +28,7 @@ date = now.strftime("%Y-%m-%d")
 data_dir = os.path.join(DATA_DIR_ROOT, date)
 os.makedirs(data_dir, exist_ok=True)
 
-for i in sp500_list.itertuples():
+for i in nyse_list.itertuples():
     print(f"작업({i.Index}): {i.Symbol} / {i.Name}")
     filename = f"{i.Symbol}-{i.Name}.csv"
     file_path = os.path.join(data_dir, filename)
@@ -93,7 +93,7 @@ def calc_score(data, day=-1):
         return -1
 
 
-for i in sp500_list.itertuples():
+for i in nyse_list.itertuples():
     print(f"작업({i.Index}): {i.Symbol} / {i.Name}")
     filename = f"{i.Symbol}-{i.Name}.csv"
     file_path = os.path.join(data_dir, filename)
@@ -148,7 +148,7 @@ rs_df[na_index]['RankChange'] = -1
 sorted = rs_df.sort_values('Rank', ascending=False)
 
 posts_dir = os.path.join("docs", "_posts")
-result_file_path = os.path.join(posts_dir, f"{date}-sp500-rs.markdown")
+result_file_path = os.path.join(posts_dir, f"{date}-nyse-rs.markdown")
 
 with open(result_file_path, "w") as f:
     header_start = '''\
@@ -156,7 +156,7 @@ with open(result_file_path, "w") as f:
     layout: single
     '''
     f.write(textwrap.dedent(header_start))
-    f.write(now.strftime('title: "SP500 상대강도 %Y년 %-m월 %-d일"\n'))
+    f.write(now.strftime('title: "NYSE 상대강도 %Y년 %-m월 %-d일"\n'))
     f.write(now.strftime("date: %Y-%m-%d %H:%M:%S +0900\n"))
     header_end = '''\
     categories: rs
@@ -165,11 +165,11 @@ with open(result_file_path, "w") as f:
     f.write(textwrap.dedent(header_end))
 
     comment = '''\
-    SP500 전 종목의 상대강도를 계산했다.
+    NYSE 전 종목의 상대강도를 계산했다.
 
     [윌리엄 오닐의 Relative Strength Rating](https://www.williamoneil.com/proprietary-ratings-and-rankings/)에 기반하여 상대 강도를 계산했다.
 
-    ## SP500 상대강도
+    ## NYSE 상대강도
     
     |티커|이름|1년 전|종가|상대강도|
     |------|---|-----|--|------|
@@ -188,7 +188,7 @@ with open(result_file_path, "w") as f:
 
 
 result_file_path = os.path.join(
-    posts_dir, f"{date}-sp500-trend-template.markdown")
+    posts_dir, f"{date}-nyse-trend-template.markdown")
 
 minervini = sorted[sorted.RS >= 70]
 minervini = minervini[minervini.Close2 > minervini.MA50]
@@ -213,7 +213,7 @@ with open(result_file_path, "w") as f:
     layout: single
     '''
     f.write(textwrap.dedent(header_start))
-    f.write(now.strftime('title: "SP500 미너비니 트렌드 템플릿 %Y년 %-m월 %-d일"\n'))
+    f.write(now.strftime('title: "NYSE 미너비니 트렌드 템플릿 %Y년 %-m월 %-d일"\n'))
     f.write(now.strftime("date: %Y-%m-%d %H:%M:%S +0900\n"))
     header_end = '''\
     categories: minervini
