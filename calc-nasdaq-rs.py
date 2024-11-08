7cf5f6f8a2c2ff322d3f68509313afa97c144573
remaster5 <right.stock.hunter@gmail.com>
2024-11-08 18:56:45 +0900

    Update calc-nasdaq-rs.py

--- calc-nasdaq-rs.py
+++ calc-nasdaq-rs.py
@@ -70,7 +70,7 @@ rs_df = pd.DataFrame(columns=[
 
 
 def c(symbol):
-    link = f"https://m.stock.naver.com/worldstock/stock/{symbol}.O/total"
+    link = f"https://finance.yahoo.com/quote/{symbol}/"
     return f"[{symbol}]({link})"
 
 
