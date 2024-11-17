88a07d075bdae3dc8fe9b7c4d2064f37c117136d
remaster5 <right.stock.hunter@gmail.com>
2024-11-17 22:44:35 +0900

    Update calc-nasdaq-rs.py

--- calc-nasdaq-rs.py
+++ calc-nasdaq-rs.py
@@ -160,7 +160,7 @@ with open(result_file_path, "w") as f:
     layout: single
     '''
     f.write(textwrap.dedent(header_start))
-    f.write(now.strftime('title: "나스닥 상대강도 %Y년 %-m월 %-d일"\n'))
+    f.write(now.strftime('title: "NASDAQ RS %Y년 %-m월 %-d일"\n'))
     f.write(now.strftime("date: %Y-%m-%d %H:%M:%S +0900\n"))
     header_end = '''\
     categories: rs
@@ -169,9 +169,9 @@ with open(result_file_path, "w") as f:
     f.write(textwrap.dedent(header_end))
 
     comment = '''\
-    나스닥 전 종목의 상대강도를 계산했다.
+    나스닥 상대강도
 
-    [윌리엄 오닐의 Relative Strength Rating](https://www.williamoneil.com/proprietary-ratings-and-rankings/)에 기반하여 상대 강도를 계산했다.
+    윌리엄 오닐 상대 강도
 
     ## 나스닥 상대강도
     
@@ -218,7 +218,7 @@ with open(result_file_path, "w") as f:
     layout: single
     '''
     f.write(textwrap.dedent(header_start))
-    f.write(now.strftime('title: "나스닥 미너비니 트렌드 템플릿 %Y년 %-m월 %-d일"\n'))
+    f.write(now.strftime('title: "NASDAQ 미너비니 %Y년 %-m월 %-d일"\n'))
     f.write(now.strftime("date: %Y-%m-%d %H:%M:%S +0900\n"))
     header_end = '''\
     categories: minervini
@@ -227,11 +227,9 @@ with open(result_file_path, "w") as f:
     f.write(textwrap.dedent(header_end))
 
     comment = '''\
-    마크 미니버니(Mark Minervini)의 트렌드 템플릿(Trend Template)을 계산하여 만족한 결과만 나열하였습니다. 필터링에 걸린 종목은 아래에 나열되어 있지 않습니다.
+    마크 미니버니 상대강도
 
-    아래 기술된 미너비니 트렌드 템플릿 계산 방식으로 계산합니다. 계산 방법에서 RS 값이 최소 70이상이고 80, 90이면 좋다고 하고 있는데 70이상만 결과로 표기하고 80이나 90에 대해서 특별히 더 자세히 보이지는 않습니다.
-
-    ## 미너비니 트렌드 템플릿
+    ## 미너비니 상대강도
     
     |티커|이름|종가|RS|신고가,신저가|MA50,150,200|
     |------|---|---|--|---------|------------|
@@ -245,17 +243,6 @@ with open(result_file_path, "w") as f:
     
     f.write("\n")
     footer = '''\
-    ## 미너비니 트렌드 템플릿 계산 방식
-
-    "Trade Like a Stock Market Wizard: How to Achieve Super Performance in Stocks in Any Market"에서
     
-     1. The current stock price is above both the 150-day (30-week) and the 200-day (40-week) moving average price lines.
-     1. The 150-day moving average is above the 200-day moving average.
-     1. The 200-day moving average line is trending up for at least 1 month (preferably 4–5 months minimum in most cases).
-     1. The 50-day (10-week) moving average is above both the 150-day and 200-day moving averages.
-     1. The current stock price is trading above the 50-day moving average.
-     1. The current stock price is at least 30 percent above its 52-week low. (Many of the best selections will be 100 percent, 300 percent, or greater above their 52-week low before they emerge from a solid consolidation period and mount a large scale advance.)
-     1. The current stock price is within at least 25 percent of its 52-week high (the closer to a new high the better).
-     1. The relative strength ranking (as reported in Investor’s Business Daily) is no less than 70, and preferably in the 80s or 90s, which will generally be the case with the better selections.
     '''
     f.write(textwrap.dedent(footer))
